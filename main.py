2de07cc858d96962cbd56c0c329f143a74866620
Harry Lloyd <harry.lloyd@bl.uk>
2026-02-27 11:59:45 +0000

    add dataset creation and post-proc fns

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 import emp.dataset as data
 
-def main():
+def create_title_loc_df():
     text = data.parse_proudfoot("data/raw/emp.pdf")
     preproc_text = data.preprocess_text(text)
     all_titles_raw = data.gen_title_lines(preproc_text)
@@ -20,15 +20,36 @@ def main():
 
     manual_check_df = pd.read_csv("data/interim/missing_title_adjacent_manual_check.csv", encoding="UTF8", index_col=0)
     title_loc_df = data.extract_clean_entries(manual_check_df, title_loc_df, desc_lines)
+    return title_loc_df
 
+def post_process_outputs(jsons, csv_out):
     header_template = pd.read_csv("data/external/Books_template.csv", nrows=2, encoding="utf8")
-    jsons = glob.glob("data/processed/model_outputs/gt_outputs/*.json")
-    json_dict = {os.path.basename(j).split(".")[0].replace("_", " ").title(): json.load(open(j)) for j in jsons}
+    jsons = glob.glob(jsons)
+    json_dict = {os.path.basename(j).split(".")[0].replace("_", " ").title().replace("Al-", "al-"): json.load(open(j)) for j in jsons}
     metadata_df = data.process_output_to_csv(json_dict)
     marc_df = data.post_process_csv(metadata_df=metadata_df, header_template=header_template)
-    marc_df.to_csv("data/processed/model_outputs/gt_outputs/gt_output_batch_postproc.csv", encoding="utf-8-sig")
+    marc_df.to_csv(csv_out, encoding="utf-8-sig")
     return title_loc_df
 
 if __name__ == "__main__":
-    title_loc_df = main()
-    print(title_loc_df.shape)
+    trial_titles = pd.read_csv("data/external/trial_harry_jan_2026.csv", encoding="utf-8", header=None)
+    trial_titles["title"] = trial_titles[1].apply(lambda x: x[:-5]).str.strip()
+
+    title_loc_df = create_title_loc_df()
+    trial_texts = title_loc_df.set_index("short_title_titles").loc[trial_titles["title"]]
+    prompts = {title: data.gen_prompt(entry_text=text, book_title=title) for title, text in zip(trial_texts.index, trial_texts["entry_text"].values)}
+
+    CREATE_PROMPTS = False
+    CREATE_JSON = False
+
+    if CREATE_PROMPTS:
+        for title, prompt in prompts.items():
+            with open(f"models/prompts/non_gt_prompts/batch_260130/{title.lower().replace(" ", "_")}.txt", "w", encoding="utf8") as f:
+                f.write(prompt)
+
+            if CREATE_JSON and not os.path.exists(f"data/processed/model_outputs/non_gt_outputs/batch_260130/{title.lower().replace(" ", "_")}.json"):
+                with open(f"data/processed/model_outputs/non_gt_outputs/batch_260130/{title.lower().replace(" ", "_")}.json", "w") as f:
+                    f.write("")
+
+    post_process_outputs(jsons="data/processed/non_gt_outputs/batch_260130/*.json", csv_out="data/processed/non_gt_outputs/batch_260130/260130_postproc.csv")
+    print("")

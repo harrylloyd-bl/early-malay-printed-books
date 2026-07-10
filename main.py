418f627bfca0f7788f60f713e7fa7df2af30bd25
Harry Lloyd <harry.lloyd@bl.uk>
2026-07-10 18:10:18 +0100

    add name mapping, batch, model type

--- main.py
+++ main.py
@@ -12,19 +12,16 @@ import emp.dataset as data
 from emp.config import *
 
 
-def post_process_outputs(text_outputs, csv_out):
+def post_process_outputs(text_outputs, name_mapping, csv_out):
     header_template = pd.read_csv("data/external/Books_template.csv", nrows=2, encoding="utf8")
     outputs = glob.glob(text_outputs)
     json_dict: dict[str, str | dict[str, list[dict[str, str]]]] = {}
     for f in outputs:
         try:
-            short_name = os.path.basename(f).split(".")[0].replace("_", " ").title().replace("Al-", "al-")
+            short_name = name_mapping[os.path.basename(f)]
             work_json = json.loads(open(f).read().strip("````").strip("json"))
             json_dict[short_name] = work_json
         except json.decoder.JSONDecodeError:
-            print(str(res[1].choices[0].finish_reason) + "\n")
-            print(str(res[1].choices[0].message.refusal) + "\n")
-            print(str(res[1].choices[0].message.content))
             json_dict[short_name] = "JSON DECODE FAILURE"
     
     metadata_df = data.process_output_to_csv(json_dict)
@@ -46,8 +43,9 @@ if __name__ == "__main__":
     
     POST_PROCESS = True
 
-    BATCH = "260623"
-    MODEL = "qwen3.6-35b-a3b"
+    BATCH = "260710"
+    MODEL = "qwen3.7-plus-2026-05-26"
+    PREPARED_ENTRIES = "interim/title_dedup_aac_emp.csv"
     
     if not os.path.exists(os.path.join(DATA_DIR, f"processed/batch_{BATCH}/")):
         os.mkdir(os.path.join(DATA_DIR, f"processed/batch_{BATCH}/"))
@@ -60,8 +58,9 @@ if __name__ == "__main__":
     else:
         title_loc_df = pd.read_csv(trial_loc_path, encoding="utf-8-sig", index_col=0)
 
-    prepared_entry_df = pd.read_csv(os.path.join(DATA_DIR, "interim/title_dedup_boll_emp.csv"), encoding="utf-8-sig").set_index("title")
-    entries = {title: data.gen_prompt(entry_text=row["entry_text"], book_title=title) for title, row in prepared_entry_df.iloc[5:].iterrows()}
+    prepared_entry_df = pd.read_csv(os.path.join(DATA_DIR, PREPARED_ENTRIES), encoding="utf-8-sig").set_index("title")
+    name_mapping = {title.lower().replace(" ", "_").replace(":", "_")+".txt": title for title in prepared_entry_df.index}
+    entries = {title: data.gen_prompt(entry_text=row["entry_text"], book_title=title) for title, row in prepared_entry_df.iloc[:].iterrows()}
 
     if USE_DEFAULT_WORKSPACE:
         base_url=f"https://{os.environ['SINGAPORE_API_HOST']}/compatible-mode/v1"
@@ -70,11 +69,16 @@ if __name__ == "__main__":
 
     if CALL_API:
         print("Running event loop...")
-        # structured_entries = asyncio.run(data.structure_all_entries(base_url=base_url, entries=entries, max_concurrent=15, model=MODEL, logger=logger))
-        for res in structured_entries:
-            with open(f"data/processed/batch_{BATCH}/{res[0].lower().replace(" ", "_").replace(":", "_")}.txt", "w") as f:
-                logging.info(f"Output token count: {len(res[1].choices[0].message.content.split(" "))}")
-                f.write(res[1].choices[0].message.content.strip("```").strip("json"))
+        structured_entries = asyncio.run(
+            data.structure_all_entries(
+                base_url=base_url,
+                entries=entries,
+                max_concurrent=5,
+                model=MODEL,
+                logger=logger,
+                batch=BATCH
+            )
+        )
 
     if CREATE_PROMPTS:
         for title, prompt in entries.items():
@@ -87,5 +91,5 @@ if __name__ == "__main__":
                     f.write("")
 
     if POST_PROCESS:
-        post_process_outputs(text_outputs=os.path.join(DATA_DIR, f"processed/batch_{BATCH}/*.txt"), csv_out=os.path.join(DATA_DIR, f"processed/batch_{BATCH}/{BATCH}_postproc.csv"))
+        post_process_outputs(text_outputs=os.path.join(DATA_DIR, f"processed/batch_{BATCH}/*.txt"), name_mapping=name_mapping,csv_out=os.path.join(DATA_DIR, f"processed/batch_{BATCH}/{BATCH}_postproc.csv"))
     print("")
